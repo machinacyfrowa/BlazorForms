c212c604cecf6d2d2c8489ab83aaa96de769aa61
Paweł Zawadzki <pawel.zawadzki2@teb.edu.pl>
2025-05-23 11:05:48 +0200

    Dodaj klasę User i formularz rejestracji
    Wprowadzono nową klasę `User` w pliku `User.cs`, która zawiera właściwości `Name`, `Email` i `Password`.
    
    W pliku `Form.razor` dodano formularz rejestracji użytkownika z polem tekstowym dla imienia oraz przyciskiem do wysyłania. Zdefiniowano metodę `OnSubmit` do obsługi formularza.

---
+++ BlazorForms/User.cs
@@ -0,0 +1,14 @@
+﻿namespace BlazorForms
+{
+    /// <summary>
+    /// Klasa reprezentująca użytkownika - podstawa do utworzenia 
+    /// formularza rejestracji.
+    /// </summary>
+    public class User
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+
+    }
+}
